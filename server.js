d0a2ed29b7e677bb47bcfab6113511fba39234f2
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-06 10:08:49 +0900

    fix: server.js sequelize 부분 삭제

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
 const signupRouter = require("./routes/signup");
 const loginRouter = require("./routes/login");
-const setupDB = require("./db_setup"); // 실제 DB 사용 시 주석 처리 후 사용
-// const { initializeDatabase } = require("./models"); // 데이터베이스 초기화 함수 가져오기
+const setupDB = require("./db_setup");
 const cors = require("cors");
 const session = require("express-session");
 const multer = require("multer");
@@ -33,43 +32,13 @@ const upload = multer({
   },
 });
 
-// CORS 설정 - 다른 도메인에서의 요청을 허용
-const corsOptions = {
-  origin: `http://localhost:5713`, // 클라이언트 포트 설정
-  credentials: true, // 세션을 사용할 경우 true로 설정
-};
-app.use(cors(corsOptions));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// 세션 설정
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET_KEY, // 원하는 시크릿 키로 변경하세요
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }, // HTTPS를 사용하는 경우 true로 설정
-  })
-);
 
 // 파일 업로드 미들웨어 설정
 app.use(upload.single("client_photo"));
 
-// // Test용 DB 사용 시
-// app.use("/signup", signupRouter);
-
-// initializeDatabase()
-//   .then(() => {
-//     // 서버 시작
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error("Failed to initialize database:", err);
-//   });
-
-// 실제 DB 사용 시
+// DB 초기화 완료 후 서버 가동
 setupDB()
   .then(({ mysqldb }) => {
     app.set("mysqldb", mysqldb);
