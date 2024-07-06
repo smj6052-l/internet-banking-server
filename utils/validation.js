fb2df89733dfb9ff77fc9042ea26ec415d6b12fc
seungjun <klsj9810@gmail.com>
2024-07-07 00:59:26 +0900

    feat: 캡챠 인증을 통한 로그인 기능 추가 및 signup.js, validation.js 개선
    - login.js 캡챠 기능 추가
    - signup.js 주민번호, 전화번호 검증울 db 엑세스 최소로 만들기 위해 1회 엑세스 후 둘 다 검증하게 설계했습니다. 악의적인 유저가 내부 정보를 파악할 수 없도록 중복 전화번호, 중복 주민번호로 회원가입 실패 시 그냥 회원가입 실패 메시지를 전송합니다.
    - validation.js 기존 주민번호, 전화번호 중복 검사 코드를 제거했습니다.

--- utils/validation.js
+++ utils/validation.js
@@ -10,30 +10,6 @@ async function checkId(mysqldb, client_id) {
   }
 }
 
-// 중복 전화번호 체크
-async function checkPhone(mysqldb, client_phone) {
-  try {
-    const [rows] = await mysqldb
-      .promise()
-      .query("SELECT * FROM Client WHERE client_phone = ?", [client_phone]);
-    return rows.length === 0;
-  } catch (error) {
-    throw new Error("Database error");
-  }
-}
-
-// 중복 주민등록번호 체크
-async function checkResi(mysqldb, client_resi) {
-  try {
-    const [rows] = await mysqldb
-      .promise()
-      .query("SELECT * FROM Client WHERE client_resi = ?", [client_resi]);
-    return rows.length === 0;
-  } catch (error) {
-    throw new Error("Database error");
-  }
-}
-
 // 이메일 인증 코드 확인
 function verifyEmailCode(sessionVerificationCode, verificationCode) {
   if (verificationCode === sessionVerificationCode) {
@@ -44,7 +20,5 @@ function verifyEmailCode(sessionVerificationCode, verificationCode) {
 
 module.exports = {
   checkId,
-  checkPhone,
-  checkResi,
   verifyEmailCode,
 };
